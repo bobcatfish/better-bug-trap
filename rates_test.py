4afa3d0f855b90c3fc431a9cd58f3edd061e3992
Christie Wilson <bobcatfish@gmail.com>
2022-01-11 18:34:09 -0800

    Change the default to 30 😳
    In a funky way

--- rates_test.py
+++ rates_test.py
@@ -8,6 +8,10 @@ class TestDailyRates(unittest.TestCase):
         r = rates.get_daily_rates("catcoin", 3)
         self.assertEqual(r, [2.0]*3)
 
+    def test_get_daily_rates_thirty_days(self):
+        r = rates.get_daily_rates("catcoin")
+        self.assertEqual(r, [2.0]*30)
+
 
 if __name__ == "__main__":
     unittest.main()
